adb8b3c66042d43139d8731c3038efe3c2d115be
Lyoko <519052532@qq.com>
2022-02-10 10:12:37 +0800

    fix: support use vue2-script-setup and lang

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,18 @@ export const unplugin = createUnplugin<Options>(() => ({
     return /\.vue$/.test(id) || /\.vue\?vue/.test(id);
   },
   transform(code, id) {
-    var component = parseComponent(code);
-    var { setup, name } = component.script?.attrs || {};
-    if (!(setup && name)) return;
-    code = `<script>export default { name: "${name}" }</script>\n${code}`;
+    const component = parseComponent(code);
+    const { attrs, content } = component.script || {};
+    const { setup, name, lang } = attrs || {};
+    const setupCompiledStr = "export default __sfc_main";
+    if (setup && name) {
+      // before compile setup
+      const scriptLang = lang ? `lang="${lang}"` : "";
+      code = `<script ${scriptLang}>\nexport default { name: "${name}" };\n</script>\n${code}`;
+    } else if (name && content?.includes(setupCompiledStr)) {
+      // after vue2-script-setup
+      code = code.replace(setupCompiledStr, `__sfc_main.name = "${name}";\n${setupCompiledStr}`);
+    }
     return { code };
     // const { descriptor } = parse(code);
     // const script = compileScript(descriptor, { id });
